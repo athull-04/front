c8a7bffa4b0d4e7433b49b1ab0c9db8b227d5806
athull-04 <athuler7@gmail.com>
2025-02-16 20:06:06 +0530

    Update api.js

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://shop-budu.onrender.com/api";
 
 export const getProducts = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/products");
+    const response = await axios.get("https://shop-budu.onrender.com/api/products");
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
