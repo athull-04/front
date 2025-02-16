1ef4b1d8266a6f5bc680fbe6f0511d287bc915f6
athull-04 <athuler7@gmail.com>
2025-02-16 20:06:23 +0530

    Update auth.js

--- front/src/services/auth.js
+++ front/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://shop-budu.onrender.com/api/auth";
 
 export const login = async (username, password) => {
   try {
