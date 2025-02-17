36aff8876cd54a26ad16a8b90de807bbcff2c9ef
athull-04 <athuler7@gmail.com>
2025-02-17 20:21:42 +0530

    Update Profile.jsx

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -17,7 +17,7 @@ const ProfilePage = () => {
     if (!refreshToken) return null;
 
     try {
-      const response = await axios.post('https://bucolic-pony-093027.netlify.app/api/auth/refresh', { refreshToken });
+      const response = await axios.post('https://backaztee.onrender.com/api/auth/refresh', { refreshToken });
       return response.data.accessToken; // Assuming the response contains a new access token
     } catch (err) {
       console.error('Error refreshing token:', err);
@@ -36,7 +36,7 @@ const ProfilePage = () => {
 
     const fetchUserData = async () => {
       try {
-        const response = await axios.get('https://bucolic-pony-093027.netlify.app/api/auth/profile', {
+        const response = await axios.get('https://backaztee.onrender.com/api/auth/profile', {
           headers: {
             Authorization: `Bearer ${token}`,
           },
